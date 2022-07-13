5682f3c07c3c59b3353fffda0bbe19993baafda5
Jonathan <kloepj@rpi.edu>
2022-07-13 11:37:02 -0400

    Fix

---
+++ alpha1/src/views/AboutView.vue
@@ -0,0 +1,8 @@
+import {chessboard} from 'vue-chessboard'
+import 'vue-chessboard/dist/vue-chessboard.css'
+<template>
+  <div class="about">
+    <chessboard/>
+    <h1>This is an about page</h1>
+  </div>
+</template>
---
+++ alpha1/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+<div id="myBoard" style="width: 400px"></div>
+  </div>
+</template>
+<script>
+    import { Chess } from 'chess.js'
+    const chess = new Chess()
+    console.log(chess.ascii())
+export default {
+  name: 'HomeView',
+}
+</script>
