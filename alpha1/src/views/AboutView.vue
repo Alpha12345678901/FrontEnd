a1bdf6906abfb8a68bbd0720584e28597fe573bb
Jonathan <kloepj@rpi.edu>
2022-07-21 16:18:00 -0400

    signup

--- alpha1/src/views/AboutView.vue
+++ alpha1/src/views/AboutView.vue
@@ -1,9 +1,11 @@
 import {chessboard} from 'vue-chessboard'
 import 'vue-chessboard/dist/vue-chessboard.css'
 <template>
+<form>
   <label for="fname">Username:</label>
   <input type="text" id="fname" name="fname"><br><br>
   <label for="lname">Password:</label>
   <input type="text" id="lname" name="lname"><br><br>
   <input type="submit" value="Signup">
+  </form>
 </template>
