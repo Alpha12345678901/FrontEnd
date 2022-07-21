760185d2f1ed0fd805d8947cd6c16dbba1840e24
Toggytokyo <42394145+Toggytokyo@users.noreply.github.com>
2022-07-21 16:16:14 -0400

    Updated Format

--- alpha1/src/views/AboutView.vue
+++ alpha1/src/views/AboutView.vue
@@ -1,8 +1,9 @@
 import {chessboard} from 'vue-chessboard'
 import 'vue-chessboard/dist/vue-chessboard.css'
 <template>
-  <div class="about">
-    <chessboard/>
-    <h1>This is an about page</h1>
-  </div>
+  <label for="fname">Username:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="lname">Password:</label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <input type="submit" value="Signup">
 </template>
