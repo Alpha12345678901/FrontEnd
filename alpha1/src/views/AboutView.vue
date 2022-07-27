b62db99c9b5e9c50e8a2a0d0a8964786a2abf024
Jonathan <kloepj@rpi.edu>
2022-07-27 10:42:13 -0400

    More Socket Work

--- alpha1/src/views/AboutView.vue
+++ alpha1/src/views/AboutView.vue
@@ -7,14 +7,15 @@ import {chessboard} from 'vue-chessboard' import
   <input type="text" id="lname" name="lname" /><br /><br />
   <input type="submit" value="Signup" />
   <input type="text" id="lname" name="lname" v-model="id" /><br /><br />
-  <p><button class="button is-primary" @click="join">Register</button></p>
-  <p><button class="button is-primary" @click="creat">Register</button></p>
+  <p><button class="button is-primary" @click="join">Join Room</button></p>
+  <p><button class="button is-primary" @click="creat">Creat Room</button></p>
+  <p v-if="code">Code: {{code}}</p>
 </template>
 <script setup>
 import { io } from "socket.io-client";
 import { ref } from "vue";
 const id = ref("");
-
+const code= ref();
 const socket = io("http://localhost:3000");
 socket.on("connect", () => {
   console.log("HI");
@@ -25,6 +26,7 @@ const join = () => {
 const creat = () => {
   socket.emit("creat", socket.id);
   console.log(socket.id);
+   code.value=socket.id;
 };
 socket.on("HI", () => {
   console.log("HI");
